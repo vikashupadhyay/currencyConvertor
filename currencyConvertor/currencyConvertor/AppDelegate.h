417d77b1c531f514ef3e28622b4c8cab52a04b09
vikashupadhyay <vikasku@thoughtworks.com>
2016-06-15 20:56:08 +0530

    added xcode

---
+++ currencyConvertor/currencyConvertor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  currencyConvertor
+//
+//  Created by kvikas on 6/15/16.
+//  Copyright © 2016 kvikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ currencyConvertor/currencyConvertor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  currencyConvertor
+//
+//  Created by kvikas on 6/15/16.
+//  Copyright © 2016 kvikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
